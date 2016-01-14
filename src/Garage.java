cafd31f177ca5458117e281c6cbfc33a603a57c3
christian-ericksen <christian.ericksen@gmail.com>
2016-01-14 12:56:19 -0500

    Blah

---
+++ src/Garage.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Christian on 1/14/16.
+ */
+public class Garage {
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Christian on 1/14/16.
+ */
+
+        public class Main {
+    public static void main(String[] args) {
+
+    }
+}
